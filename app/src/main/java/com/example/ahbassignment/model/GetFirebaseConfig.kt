a31f02be74b0e8627d671ffd975dee9a9cf739df
Shaheer <shaheer.shamsudheen@adib.com>
2022-02-17 02:28:06 +0400

    initial commit

---
+++ app/src/main/java/com/example/ahbassignment/model/GetFirebaseConfig.kt
@@ -0,0 +1,10 @@
+package com.example.ahbassignment.model
+
+/**
+ * Created by Shaheer cs on 16/02/2022.
+ */
+data class GetFirebaseConfig(
+    val maintenanceStatus: Boolean,
+    val bannerTextEN: String,
+    val bannerTextARA: String,
+)
